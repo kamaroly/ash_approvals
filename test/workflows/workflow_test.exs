131a0ac6d00c0a8956c64b8b49d31a39eb4d6944
Kamaro <kamaroly@gmail.com>
2025-11-15 21:44:40 +0300

    Added tables for workflow test

---
+++ test/workflows/workflow_test.exs
@@ -0,0 +1,164 @@
+defmodule Workflows.WorkflowTest do
+  use ExUnit.Case
+
+  defmodule Workflow do
+    use Ash.Resource,
+      domain: Workflows.WorkflowTest.Domain,
+      data_layer: Ash.DataLayer.Ets
+
+    ets do
+      table :workflows
+    end
+
+    code_interface do
+      define :create, action: :create
+    end
+
+    actions do
+      default_accept [:name, :description, :status]
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+      attribute :name, :string, allow_nil?: false
+      attribute :description, :string
+
+      attribute :status, :atom do
+        default :active
+      end
+
+      timestamps()
+    end
+  end
+
+  defmodule WorkflowSteps do
+    use Ash.Resource,
+      domain: Workflows.WorkflowTest.Domain,
+      data_layer: Ash.DataLayer.Ets
+
+    ets do
+      table :workflow_steps
+    end
+
+    actions do
+      defaults [:read, :update, :destroy, create: :*]
+    end
+
+    attributes do
+      uuid_primary_key :id
+      attribute :step_order, :integer
+      attribute :step_name, :string
+
+      attribute :approver_type, :atom do
+        constraints one_of: [:user, :group, :specific, :manager, :managers_manager]
+        default :user
+      end
+
+      attribute :approver_id, :uuid
+
+      attribute :approval_mode, :atom do
+        constraints one_of: [:sequential, :paralle]
+        default :sequential
+      end
+
+      attribute :conditions, :string
+      attribute :timeout_days, :integer
+      attribute :required, :boolean
+    end
+
+    relationships do
+      belongs_to :template, Workflow, allow_nil?: false
+    end
+  end
+
+  defmodule Requests do
+    use Ash.Resource,
+      domain: Workflows.WorkflowTest.Domain,
+      data_layer: Ash.DataLayer.Ets,
+      extensions: [AshApprovals]
+
+    ets do
+      table :requests
+    end
+
+    actions do
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      attribute :request_type, :atom do
+        constraints one_of: [:initiate, :change]
+      end
+
+      attribute :status, :atom do
+        constraints one_of: [:pending, :approved, :rejected, :in_progress, :completed]
+      end
+
+      attribute :request_data, :map
+      create_timestamp :created_at
+      update_timestamp :updated_at
+    end
+
+    relationships do
+      belongs_to :template, Workflow, allow_nil?: false
+      belongs_to :current_step, WorkflowSteps
+    end
+  end
+
+  defmodule Approvals do
+    use Ash.Resource,
+      domain: Workflows.WorkflowTest.Domain,
+      data_layer: Ash.DataLayer.Ets
+
+    ets do
+      table :approvals
+    end
+
+    actions do
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      attribute :status, :atom do
+        constraints one_of: [:approved, :rejected, :pending]
+        default :pending
+      end
+
+      attribute :comments, :string
+      create_timestamp :approved_at
+    end
+
+    relationships do
+      belongs_to :request, Requests, allow_nil?: false
+      belongs_to :step, WorkflowSteps, allow_nil?: false
+    end
+  end
+
+  # Update the domain to include the new resources
+  defmodule Domain do
+    use Ash.Domain, validate_config_inclusion?: false
+
+    resources do
+      resource Workflows.WorkflowTest.Workflow
+      resource Workflows.WorkflowTest.WorkflowSteps
+      resource Workflows.WorkflowTest.Requests
+      resource Workflows.WorkflowTest.Approvals
+    end
+  end
+
+  describe "Workflow testing" do
+    test "it should create workflow" do
+      Workflow.create!(%{
+        name: "Expense Approval",
+        description: "Workflow for approving expenses",
+        status: :status
+      })
+      |> dbg()
+    end
+  end
+end
